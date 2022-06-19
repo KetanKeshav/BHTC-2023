6b47495a1234a3665a643018558f951bee630acd
anzar00 <peerzada.anzar@gmail.com>
2022-06-19 19:40:36 +0530

    initial setup

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <title>BHTC</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
-    
+
   <?php include 'header.php' ?>
 </head>
 
@@ -17,7 +17,7 @@
   <?php include 'navbar.php' ?>
 
   <!-- ======= Hero Section ======= -->
-  <section id="hero" class="d-flex align-items-center">
+  <section id="hero" class="align-items-center">
     <div id="carouselExampleInterval" class="carousel slide carousel-fade" data-bs-ride="carousel">
       <div class="carousel-inner">
         <div class="carousel-item slide-1 active" data-bs-interval="7000">
@@ -42,109 +42,61 @@
     </div>
   </section>
   <!-- End Hero -->
-  
-  
+
+
   <main id="main">
-
-
-    <!-- ======= Related Topics Section ======= -->
-    <!-- <section id="related" class="related section-bg">
-      <div class="container">
-
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="section-title" data-aos="fade-right">
-              <h2>Realated Topics</h2>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-              
-              <div class="col-md-4 align-items-center mt-4 mt-lg-0">
-                <div class="icon-box" data-aos="zoom-in" data-aos-delay="200">
-                  <div class="icon"><i class="bx bx-file"></i></div>
-                  <h4><a href="">Chemicals and Waste</a></h4>
-                  <p>Related Goals</p>
-                </div>
-              </div>
-
-              <div class="col-md-4 align-items-center mt-4 mt-lg-0">
-                <div class="icon-box" data-aos="zoom-in" data-aos-delay="200">
-                  <div class="icon"><i class="bx bx-file"></i></div>
-                  <h4><a href="">Sustainable consumption and production</a></h4>
-                  <p>Related Goals</p>
-                </div>
-              </div>
-
-              <div class="col-md-4 align-items-center mt-4 mt-lg-0">
-                <div class="icon-box" data-aos="zoom-in" data-aos-delay="200">
-                  <div class="icon"><i class="bx bx-file"></i></div>
-                  <h4><a href="">Sustainable tourism</a></h4>
-                  <p>Related Goals</p>
-                </div>
-              </div>
-              
-        </div>
-
-      </div>
-    </section> -->
-    <!-- End Related Topics Section -->
 
     <!-- ======= Overview Section ====== -->
     <section id="overview" class="overview section-bg">
       <div class="overview-nav">
         <ul class="nav nav-tabs" id="myTab" role="tablist">
           <li class="nav-item" role="presentation">
-            <button class="nav-link active" id="home-tab" data-bs-toggle="tab" 
-            data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" 
-            aria-selected="true">Overview</button>
+            <button class="nav-link active" id="home-tab" data-bs-toggle="tab" data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" aria-selected="true">Overview</button>
           </li>
         </ul>
-    </div>
+      </div>
       <div class="container">
         <div class="tab-content" id="myTabContent">
           <div class="tab-pane fade show active mt-5 section-title" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
-            <h2>Overview</h2>
+            <h2>About The Event</h2>
             <p>
-            Welcome to the 2nd edition of <strong> Bangalore Humanitarian Technology Conclave (BHTC) </strong> on the theme 
-            <strong>SDG-12: Responsible Production and Consumption</strong>. 
-            <br><br>
-            <strong>The IEEE B-HTC 2022</strong> is a <strong>flagship</strong> event of IEEE Bangalore Section. It is held annually as part of 
-            the Humanitarian  outreach program of the Section; where technology leaders, innovators, 
-            entrepreneurs, and thought provokers share their passion in addressing impactful problems, 
-            and vision on futuristic technologies that will/may disrupt/impact our way of working and living.
-            <br><br>
-            This edition of IEEE BHTC will be based on the theme of SDG-12 which is meant to ensure good use of 
-            resources, improving energy efficiency, sustainable infrastructure, and providing access to basic 
-            services, green and decent jobs and ensuring a better quality of life for all, with a specific focus 
-            on: <br><br>
-            <ol>
-              <li>Sustainable management and use of natural resources</li>
-              <li>Responsible management of chemicals and food waste</li>
-              <li>Substantially reduce waste generation</li>
-              <li>Encourage companies to adopt sustainable practices and sustainability reporting</li>
-            </ol>
+              IEEE B-HTC 2023 is a premier annual cross disciplinary conference that brings together technologists,
+              engineers, scientists and investors as well as representatives from NGOs, government, academia, and
+              the industry. The conference aims at promoting research and development on “Sustainable Development
+              Goal 3: Good health and well-being” to ensure healthy life and promote well-being at all ages.
+              Before the pandemic, major progress was made in improving the health of millions of people.
+              Significant strides were made in increasing life expectancy and reducing some of the common killers
+              associated with child and maternal mortality. But more efforts are needed to fully eradicate a wide
+              range of diseases and address many different persistent and emerging health issues.
             </p>
-            <div class="container" data-aos="fade-in" data-aos-delay="200 ">
-              <img src="./assets/img/SDG.png" class="img-fluid">
-            </div>
-            
-          </div>
+            <br>
+            <p>
+              B-HTC 2023 will have plenary talks, keynote addresses by eminent experts, tutorials & workshops
+              by domain experts, research paper presentations, events for Women in Engineering and Young
+              Professionals, session on entrepreneurship and innovation, Industry exhibits and booths.
+            </p>
+            <br>
+            <p>
+              B- HTC 2023 will be held at NIE Institute of Technology, Mysuru, Karnataka, India from 24th-26th
+              March, 2023 We are delighted to announce that the IEEE Mysuru subsection and the Bangalore Section
+              are co-hosting this prestigious conference.
+            </p>
           </div>
         </div>
+      </div>
       </div>
     </section>
     <!-- End Overview Section -->
 
     <!-- ======= Events Section ======= -->
-    <section id="events" class="events">
+    <!-- <section id="events" class="events">
       <div class="container">
 
         <div class="row">
           <div class="col-lg-4">
             <div class="section-title" data-aos="fade-right">
               <h2>Tracks</h2>
-              <!-- <p>All Events go here.</p> -->
+               <p>All Events go here.</p> 
             </div>
           </div>
           <div class="col-lg-8">
@@ -154,8 +106,7 @@
                 <div class="member" data-aos="zoom-in" data-aos-delay="100">
                   <div class="member-info">
                     <h4 style="text-align: left;">Sustainable management and use of natural resources</h4>
-                    <!-- <span>Brief</span>
-                    <p>Summary</p> -->
+                    
                   </div>
                 </div>
               </div>
@@ -164,8 +115,7 @@
                 <div class="member" data-aos="zoom-in" data-aos-delay="100">
                   <div class="member-info">
                     <h4 style="text-align: left;">Responsible management of chemicals and food waste</h4>
-                    <!-- <span>Brief</span>
-                    <p>Summary</p> -->
+                    
                   </div>
                 </div>
               </div>
@@ -174,8 +124,7 @@
                 <div class="member" data-aos="zoom-in" data-aos-delay="100">
                   <div class="member-info">
                     <h4 style="text-align: left;">Substantially reduce waste generation</h4>
-                    <!-- <span>Brief</span>
-                    <p>Summary</p> -->
+                    
                   </div>
                 </div>
               </div>
@@ -184,8 +133,7 @@
                 <div class="member" data-aos="zoom-in" data-aos-delay="100">
                   <div class="member-info">
                     <h4 style="text-align: left;">Encourage companies to adopt sustainable practices and sustainability reporting</h4>
-                    <!-- <span>Brief</span>
-                    <p>Summary</p> -->
+                    
                   </div>
                 </div>
               </div>
@@ -196,28 +144,17 @@
         </div>
 
       </div>
-    </section>
+    </section> -->
     <!-- End Events Section -->
 
     <!-- ======= Publication Section ======= -->
-    <section id="publication" class="publication">
+    <!-- <section id="publication" class="publication">
       <div class="container">
 
         <div class="section-title" data-aos="fade-left">
           <h2>Speakers</h2>
           <p>All speakers here.</p>
         </div>
-
-        <!-- <div class="row" data-aos="fade-up" data-aos-delay="100">
-          <div class="col-lg-12 d-flex justify-content-center">
-            <ul id="publication-flters">
-              <li data-filter="*" class="filter-active">All</li>
-              <li data-filter=".filter-c1">C1</li>
-              <li data-filter=".filter-c2">C2</li>
-              <li data-filter=".filter-c3">C3</li>
-            </ul>
-          </div>
-        </div> -->
 
         <div class="row publication-container" data-aos="fade-up" data-aos-delay="200">
 
@@ -263,7 +200,7 @@
         </div>
 
       </div>
-    </section>
+    </section> -->
     <!-- End publication Section -->
 
     <!-- ======= OC Section ======= -->
@@ -278,7 +215,7 @@
 
           <div class="col-lg-4 col-md-6 publication-item">
             <div class="publication-wrap">
-            <img src="assets/img/oc/1.jpg" class="img-fluid" alt="">
+              <img src="assets/img/oc/1.jpg" class="img-fluid" alt="">
               <div class="publication-info">
                 <h4>C1</h4>
                 <p>c1</p>
@@ -291,7 +228,7 @@
 
           <div class="col-lg-4 col-md-6 publication-item">
             <div class="publication-wrap">
-            <img src="assets/img/oc/2.jpg" class="img-fluid" alt="">
+              <img src="assets/img/oc/2.jpg" class="img-fluid" alt="">
               <div class="publication-info">
                 <h4>C3</h4>
                 <p>c3</p>
@@ -304,7 +241,7 @@
 
           <div class="col-lg-4 col-md-6 publication-item">
             <div class="publication-wrap">
-            <img src="assets/img/oc/1.jpg" class="img-fluid" alt="">
+              <img src="assets/img/oc/1.jpg" class="img-fluid" alt="">
               <div class="publication-info">
                 <h4>C2</h4>
                 <p>c2</p>
@@ -326,7 +263,7 @@
   </main>
   <!-- End #main -->
 
-<?php include 'footer.php' ?>
+  <?php include 'footer.php' ?>
 
 </body>
 
--- navbar.php
+++ navbar.php
@@ -8,52 +8,10 @@
             <li><a class="nav-link scrollto" href="#overview">About The Event</a></li>
             <li><a class="nav-link scrollto" href="#">Sponsorships</a></li>
             <li><a class="nav-link scrollto" href="#">Agenda</a></li>
+            <li><a class="nav-link scrollto" href="#">Speakers</a></li>
             <li><a class="nav-link scrollto" href="#oc">Organizing Committee</a></li>
-            <li class="dropdown"><a href="#"><span>Previous Edition</span> <i class="bi bi-chevron-down"></i></a>
-              <ul>
-                <li><a href="https://attend.ieee.org/r10htc-2021/">R10 HTC</a></li>
-                <li><a href="https://bhtc.ieeebangalore.org/">BHTC 2020</a></li>
-              </ul>
-            </li>
             <li><a class="nav-link scrollto" href="#">Registration</a></li>
-            <li><a class="nav-link scrollto" href="#">Contact</a></li>
-            
-            <!-- <li class="dropdown"><a href="#"><span>Intergovernmental Processes</span> <i class="bi bi-chevron-down"></i></a>
-              <ul>
-                <li><a href="#">High-level Political Forum on Sustainable Development</a></li>
-                <li><a href="#">UN Conferences and High-Level Events related to sustainable development</a></li>
-                <li><a href="#">Multi-stakeholder Forum on Science, Technology and Innovation for the SDGs</a></li>
-                <li><a href="#">Second Committee of the UN General Assembly ⭧</a></li>
-                <li><a href="#">SAMOA Pathway</a></li>
-                <li><a href="#">ECOSOC Partnership Forum ⭧</a></li>
-              </ul>
-            </li>
-            <li><a class="nav-link scrollto" href="#">HLPF</a></li>
-            <li class="dropdown"><a href="#"><span>SIDS</span> <i class="bi bi-chevron-down"></i></a>
-              <ul>
-                <li><a href="#">Small Island Developing States</a></li>
-                <li><a href="#">Multidimensional Vulnerability Index for SIDS</a></li>
-                <li><a href="#">United Nations SIDS Partnerships Awards</a></li>
-              </ul>
-            </li>
-            <li class="dropdown"><a href="#"><span>Partnerships</span> <i class="bi bi-chevron-down"></i></a>
-              <ul>
-                <li><a href="#">About Partnerships</a></li>
-                <li><a href="#">The Partnership Platform</a></li>
-                <li><a href="#">ECOSOC Partnership Forum 2022</a></li>
-              </ul>
-            </li>
-            <li class="dropdown"><a href="#"><span>Engage</span> <i class="bi bi-chevron-down"></i></a>
-              <ul>
-                <li><a href="#">Events</a></li>
-                <li><a href="#">Webinars</a></li>
-                <li><a href="#">Member States</a></li>
-                <li><a href="#">UN System</a></li>
-                <li><a href="#">Stakeholder Engagement</a></li>
-              </ul>
-            </li>
-            <li><a class="nav-link scrollto" href="#">News</a></li> -->
-            
+            <li><a class="nav-link scrollto" href="#">Contact</a></li>            
           </ul>
           <i class="bi bi-list mobile-nav-toggle"></i>
         </nav>
