8e30368137f904b3b12b58d08544c7b6a6fc4767
Shazib-Wani <shazib20cs202@newhorizonindia.edu>
2022-06-29 20:48:48 +0530

    sliderfinal

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>BHTC</title>
+  <title>BHTC 2023</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
