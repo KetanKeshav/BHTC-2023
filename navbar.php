6a5eff24523fe312708ddab6e3a91cf9218b0990
Shazib-Wani <shazib20cs202@newhorizonindia.edu>
2022-07-10 16:20:29 +0530

    CFA

--- navbar.php
+++ navbar.php
@@ -5,13 +5,15 @@
         <nav id="navbar" class="navbar">
           <ul>
             <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-            <li><a class="nav-link scrollto" href="#overview">About The Event</a></li>
-            <li><a class="nav-link scrollto" href="#">Sponsorships</a></li>
+            <li><a class="nav-link scrollto" href="./assets/downloads/BHTC_2023.pdf" target="_blank" download="BHTC 2023">Call For Papers</a></li>
+            <li><a class="nav-link scrollto" href="#oc">Organizing Committee</a></li>
+            <li><a class="nav-link scrollto" href="#">Fun & Facts</a></li>
             <li><a class="nav-link scrollto" href="#">Agenda</a></li>
             <li><a class="nav-link scrollto" href="#">Speakers</a></li>
-            <li><a class="nav-link scrollto" href="#oc">Organizing Committee</a></li>
-            <li><a class="nav-link scrollto" href="#">Registration</a></li>
-            <li><a class="nav-link scrollto" href="#">Contact</a></li>            
+            <li><a class="nav-link scrollto" href="#">Registrations</a></li>
+            <li><a class="nav-link scrollto" href="#">Sponsorships</a></li>
+            <li><a class="nav-link scrollto" href="#">Contact Us</a></li>            
+            <!-- <li><a class="nav-link scrollto" href="#overview">About The Event</a></li> -->
           </ul>
           <i class="bi bi-list mobile-nav-toggle"></i>
         </nav>
