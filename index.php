35b9c25936aca22609ba831b605db1a0163f6fca
Shazib-Wani <shazib20cs202@newhorizonindia.edu>
2022-08-15 13:48:40 +0530

    CFP

--- index.php
+++ index.php
@@ -87,6 +87,131 @@
       </div>
     </section>
     <!-- End Overview Section -->
+    <!-- ======= Call For Papers Section Section ======= -->
+    <section id="callforpapers" class="callforpapers section-bg">
+      <div class="callforpapers-nav">
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+          <li class="nav-item" role="presentation">
+            <button class="nav-link active" id="home-tab" data-bs-toggle="tab" 
+            data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" 
+            aria-selected="true">Call For Papers</button>
+          </li>
+        </ul>
+    </div>
+      <div class="container">
+        <div class="tab-content" id="myTabContent">
+          <div class="tab-pane fade show active mt-5 section-title" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
+            <h2>Call For Papers</h2>
+            <p align="justify">
+            <strong> Dear Researcher</strong>,
+            <br>
+On behalf of IEEE Bangalore Humanitarian Technology Conference - BHTC 2023 organizing committee, we take this opportunity to invite you to participate in flagship humanitarian conference of IEEE Bangalore Section which is premier annual cross disciplinary conference that brings together technologists, engineers, scientists, and investors as well as representatives from NGOs, government, academia, and the industry. The conference aims at promoting research and development on <strong>“Sustainable Development Goal 3: Good health and well-being”</strong> to ensure healthy life and promote well-being at all ages. This second edition of BHTC 2023 is scheduled for on March 24-25, 2023 and is co-hosted by IEEE Mysuru Subsection.
+<br>
+<br>
+A Call-for-paper is enclosed for your perusal. We assure you of an international technically enriched and vibrant experience at BHTC 2023, and we are sure your experience at this conference will be rewarding and memorable one.
+<br>
+<br>
+Dr. Aloknath De & Dr. Sudarshan Patil Kulkarni
+<br>
+General Chairs, IEEE BHTC 2023
+
+            <br><br>
+            <strong>Topics of Interest: </strong> In recent times, significant strides are made in increasing life expectancy and reducing some of the common killers associated with child and maternal mortality. But more efforts are needed to fully eradicate a wide range of diseases and address many different persistent and emerging health issues. 
+Paper submissions are encouraged with respect to the usage of technology (sensors, algorithms, devices, and services) that helps the following domains, but are not limited to:
+<br>
+              <ol><strong>Track-1: Physiological and mental health awareness</strong>
+              <li>Promoting health awareness</li>
+              <li>Mental health care</li>
+              </ol>
+              <ol><strong>Track-2: Neonatal, Geriatrics and other specialized Healthcare</strong>
+              <li>Neonatal and Geriatric health care</li>
+              <li>Safety of health workers</li>
+              </ol>
+              <ol><strong>Track-3: Technology around Food and Nutrition</strong>
+              <li>
+
+Quality check and tracking of food products
+</li>
+              <li>Detection and control of Malnutrition</li>
+              </ol>
+              <ol><strong>Track-4: Tech-Enabling Healthcare Services</strong>
+              <li>Telehealth monitoring.</li>
+              <li>Mental health careProvision of quality essential healthcare services at an affordable price></li>
+            </ol>
+              <ol><strong>Track-5: Mitigating Health Hazards</strong>
+              <li>Pollution monitoring and control as well as its related health hazards</li>
+              <li>Road accident monitoring and reduction</li>
+              </ol>
+              <ol><strong>Track-6: Control and Monitoring of Risks and Diseases</strong>
+              <li>Control and monitoring of communicable and non-communicable diseases</li>
+              <li>Early warning for risk reduction and management of national and global health risks</li>
+              </ol>
+              <ol><strong>Track-7: Medical Devices, Equipment and other products</strong>
+              </ol>
+            </p>
+           
+            
+          </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- End Call For Papers Section -->
+
+    <!-- ======= Guidelines To Authors Section ======= -->
+
+    <section id="overview" class="overview section-bg">
+      <div class="overview-nav">
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
+          <li class="nav-item" role="presentation">
+            <button class="nav-link active" id="home-tab" data-bs-toggle="tab" 
+            data-bs-target="#home-tab-pane" type="button" role="tab" aria-controls="home-tab-pane" 
+            aria-selected="true">Guidelines</button>
+          </li>
+        </ul>
+    </div>
+      <div class="container">
+        <div class="tab-content" id="myTabContent">
+          <div class="tab-pane fade show active mt-5 section-title" id="home-tab-pane" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
+            <h2>Guidelines to Authors</h2>
+            <p align="justify">
+            Below are some useful instructions to assist the authors to submit their papers for BHTC 2023 - 
+            <br>
+            <br>
+            Papers submitted to BHTC 2022 shall contain original work by the author(s) that have not been published or submitted for publication elsewhere. The IEEE anti- plagiarism policy is applicable to all submissions. The author(s) are solely responsible for any plagiarized submission.
+
+<br>
+<br>
+<strong>Paper Submission:</strong> All papers are required to be prepared and submitted in the IEEE Standard two –column conference format of A4 size in English. Papers must be four to six pages in length, including texts, Figures, photographs and references. Any submitted paper that exceeds six pages will be rejected. The first page must include the title of the paper, author(s), affiliations, address and text. Please do not include page numbers on submitted documents. Failing to conform to the standard format will result in rejection. Detailed format information is available in IEEE Website.
+<br>
+The authors are required to use the standard IEEE manuscript template for conferences. The Microsoft Word template in can be downloaded from IEEE conference template page: Click here
+<br>
+<br>
+<strong>NOTE:</strong> Once the paper is submitted, we shall start reviewing it, so authors cannot update/resubmit the paper. They should submit the final version, update/resubmissions are not allowed.
+<br>
+<br>
+The paper shall consist of original and previously unpublished work, including specific results. The ‘Introduction’ of the paper shall clearly indicate the unique aspects of the submission and how it is related to the previous work.
+<br>
+<br>
+All paper submissions must be electronic, using IEEE Xplore compliant PDF format (*.pdf). Failure to do so may result in the rejection of the paper. The detailed template for paper submission can be found here - https://www.ieee.org/conferences/publishing/templates.html 
+  <br>
+  <br>
+Type 3 fonts (bitmaps) will not be accepted. Authors can use the IEEE PDF eXpress to generate compliant PDF Files for final submission and details for the same would be shared after the review process with all the authors.
+            <br><br>
+            <strong>Paper Upload:</strong> 
+            All papers must be submitted in PDF or as a Microsoft Word Document via Microsoft Conference Management Toolkit : https://cmt3.research.microsoft.com/IEEEBHTC2023/ 
+            <br><br>
+The submitting authors may be required to create a Microsoft Conference Management Toolkit account before uploading the paper (if the submitting author does not have a Microsoft Conference Management Toolkit account). While uploading the paper, the authors should provide the title of the paper, an abstract of no more than 250 words and keywords in the respective text boxes in the paper submission page.
+<br><br>
+Please note that all accepted papers that are registered and presented in the conference will be sent for possible inclusion in IEEE Xplore. The authors must agree to the IEEE copyright conditions and sign the IEEE copyright form as part of the online submission process. All submitted papers will be peer reviewed by the Technical Program Committees (TPC).
+<br><br>         
+          </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- ======= End Of Guidelines Section ======= -->
 
     <!-- ======= Events Section ======= -->
     <!-- <section id="events" class="events">
