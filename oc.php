a465f87ad6090767f34878589525b75fdc346948
Shazib-Wani <shazib20cs202@newhorizonindia.edu>
2022-08-23 23:01:49 +0530

    navbar

--- oc.php
+++ oc.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Organizing Committee | BHTC 2022</title>
+    <title>Organizing Committee | BHTC 2023</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
